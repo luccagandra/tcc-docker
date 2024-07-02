9644b4295c621ac4febc44e07b9a7be848ab2885
luccagandra <luccagandra@poli.ufrj.br>
2024-07-02 07:47:20 +0200

    Model now has a new level and right inertias

--- scripts/plot_forces.py
+++ scripts/plot_forces.py
@@ -3,6 +3,7 @@
 import rospy
 from gazebo_msgs.msg import ContactsState
 from geometry_msgs.msg import Vector3
+from matplotlib import pyplot as plt
 
 class ListenContact:
     def __init__(self):
@@ -13,18 +14,31 @@ class ListenContact:
 
         self.pub = rospy.Publisher('/rod/force_vector', Vector3, queue_size=100)
         self.pub2 = rospy.Publisher('/rod/torque_vector', Vector3, queue_size=100)
-        
+
+        plt.ion()
+        plt.show()
         rospy.spin()
 
     def callback(self, msg):
 
+        stamp = msg.header.stamp
+        time = stamp.secs + stamp.nsecs * 1e-9
+        
         if len(msg.states) > 0:
             force = msg.states[0].total_wrench.force
             torque = msg.states[0].total_wrench.torque
 
+            self.plot(force)
+            print("1")
             self.publish_force_and_torque(force, torque)
         else:
             pass
+
+    def plot(self, force):
+        plt.plot(force[0], '*')
+        plt.axis("equal")
+        plt.draw()
+        plt.pause(0.00000000001)
 
     def publish_force_and_torque(self, force, torque):
         
